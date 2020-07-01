c248a02f608d27205770e8ccd539a46770c994e8
Sarah Kerr <sarah.kerr93@gmail.com>
2020-06-30 21:18:17 -0400

    set up display functionality when the user searches a term

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,28 @@
 
-// var searchTerm = "Richmond";
-// var APIkey = "af1fa601daa4fd5df6a18a13cf8f70d9";
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=Richmond&appid=af1fa601daa4fd5df6a18a13cf8f70d9"
+var searchTerm = "Richmond";
+var APIkey = "af1fa601daa4fd5df6a18a13cf8f70d9";
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=" + APIkey; 
 var weatherData; 
+var cityHeader = $(".cityHeader");
+
 
 function pullWeatherData() {
+
+    $(".infoSection").empty(); 
 
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-
-      // Creating a div to hold the city details 
-      // var cityDiv = $("<div class='cityDetails'>");
-        console.log(response); 
+    
+        console.log(response);
+        var cityDiv = $("<div class='cityDetails'>");
+        var tempF = (response.main.temp - 273.15) * 9/5 + 32; 
+        var tempFRounded = Math.round(tempF * 10) / 10
+        cityHeader.text(searchTerm); 
+        cityDiv.text("Current temp: " + tempFRounded); 
+        $(".infoSection").append(cityHeader);
+        $(".infoSection").append(cityDiv);
 
     });
 
